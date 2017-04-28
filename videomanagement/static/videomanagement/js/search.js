72b65856189f781e213dea48371652255539f21a
liu6t <liutong616@hotmail.com>
2017-04-28 15:39:11 -0400

    fix look of some pages

--- videomanagement/static/videomanagement/js/search.js
+++ videomanagement/static/videomanagement/js/search.js
@@ -16,13 +16,16 @@ $( document ).ready(function() {
 	        // handle a successful response
 	        success : function(data) {
 	        	$('#videos').empty();
+	        	if(data.videos.length == 0) {
+	        		$('#videos').append("<p>Sorry, but no publicly available footage matches the time and place you provided.</p>");
+	        	}
 	        	for (var i = 0; i < data.videos.length; i++) {
 	        		var video = data.videos[i];
 		        	$('#videos').append("<div class=\"col-md-2 text-center\">" +
 		        			"<div class=\"row\"><a href=\"view_video\\" + video.id + "\">" +
 		        			"<img class= \"img-thumbnail\" width=\"100\" height=\"100\" alt=\"PoliceVideo\" src=\"gif\\" + video.id + "\">" +
 		        			"</a></div><div class=\"row\">" +
-		        			"<a href=\"view_video\\" + video.id + "\">Date:" + video.video_date + " Location:" + video.location + "</a>" +
+		        			"<a href=\"view_video\\" + video.id + "\">" + video.id + ". " + video.video_date + ". " + video.location + "</a>" +
 		        			"</div></div>");
 	        	}
 	        }
